1b904bc597770b7d2e8a84128ce354c6688f327d
Sathwika Thatiparthi <sathwika21n@gmail.com>
2026-03-03 18:05:05 -0600

    adding breadth first algorithm

--- search.py
+++ search.py
@@ -148,7 +148,7 @@ def cost_search(problem, heuristic=lambda x,y: 0):
             elif child in pqueue:
                 old_node = pqueue.get(child)
                 if child.gcost < old_node.gcost:
-                    pqueue.udpate(child)
+                    pqueue.update(child)
     return []
 
 
@@ -176,7 +176,31 @@ def breadthFirstSearch(problem):
     Search the shallowest nodes in the search tree first.
     [2nd Edition: p 73, 3rd Edition: p 82]
     """
-    return search(problem, util.Queue())
+    start = problem.getStartState()
+    if problem.isGoalState(start):
+        return []
+
+    frontier = util.Queue()
+    frontier.push(Node(start, []))
+    frontier_states = set([start])
+    explored = set()
+
+    while frontier:
+        node = frontier.pop()
+        frontier_states.remove(node.state)
+
+        if problem.isGoalState(node.state):
+            return node.actions
+
+        explored.add(node.state)
+
+        for successor, action, step_cost in problem.getSuccessors(node.state):
+            if successor in explored or successor in frontier_states:
+                continue
+            frontier.push(Node(successor, node.actions + [action]))
+            frontier_states.add(successor)
+
+    return []
 
 
 def uniformCostSearch(problem):
